b71bf874ef892b9a1ffac253feac334fefeb946a
Per Olsen <persteen22@gmail.com>
2026-02-04 15:22:05 +0100

    Added new settings for deployment to Vercel

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -31,11 +31,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY=os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
 # With DEBUG=False 404 errors will be directed to a default 404 template
 # Not: Django will display a 404.html if you have created one :-)
-# DEBUG = False
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
@@ -81,7 +81,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "vercel_app.wsgi.application"
+WSGI_APPLICATION = "vercel_app.wsgi.app"
 
 
 # Database
